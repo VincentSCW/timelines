e9cdfb9da3e5b31289bcf0acaf401b4996847a60
Vincent Shen <chengwei.shen>
2018-05-25 08:51:37 +0800

    insert and query moments

--- content_module/azure_integration/table_service.py
+++ content_module/azure_integration/table_service.py
@@ -1,17 +1,37 @@
+from datetime import date
 from azure.cosmosdb.table.tableservice import TableService
 from azure.cosmosdb.table.models import Entity
-from moment_entity import Moment
 
 class AzureTableService(object):
-    def init_table_service(self):
-        return TableService(account_name='timelines', 
+    moment_table_name = 'moment'
+
+    def __init__(self):
+        self.__table_service = TableService(account_name='timelines', 
             account_key='1Xrf8KQwttk+RzGHNBGpMrO6d7llWY6gHpyeSQ+XV6h2N3VEEtTRuSSmvt5+xA3kyQBWND1OAdt8uvMExZz1CA==',
             connection_string='DefaultEndpointsProtocol=https;AccountName=timelines;AccountKey=1Xrf8KQwttk+RzGHNBGpMrO6d7llWY6gHpyeSQ+XV6h2N3VEEtTRuSSmvt5+xA3kyQBWND1OAdt8uvMExZz1CA==;EndpointSuffix=core.chinacloudapi.cn')
 
     def create_table_if_not_exists(self, table_name):
-        table_service = self.init_table_service()
-        if table_service.exists(table_name):
+        if self.__table_service.exists(table_name):
             print('already')
         else:
-            table_service.create_table(table_name)
+            self.__table_service.create_table(table_name)
             print('ok')
+    
+    def add_moment(self, topic, record_date, content):
+        if not isinstance(record_date, date):
+            raise TypeError('record_date should be date type')
+
+        self.create_table_if_not_exists(AzureTableService.moment_table_name)
+        moment = Entity()
+        moment.PartitionKey = topic
+        moment.RowKey = record_date.strftime('%Y-%m-%d')
+        moment.content = content
+        self.__table_service.insert_entity(AzureTableService.moment_table_name, moment)
+    
+    def query_moments(self, topic):
+        moments = self.__table_service.query_entities(AzureTableService.moment_table_name,
+            filter="PartitionKey eq '%s'" % (topic,))
+        for m in moments:
+            print(m.PartitionKey)
+            print(m.RowKey)
+            print(m.content)
--- content_module/views.py
+++ content_module/views.py
@@ -1,5 +1,9 @@
+from datetime import date
 from django.shortcuts import render
+from content_module.azure_integration.table_service import AzureTableService
 
 # Create your views here.
 def index(request):
+    table_service = AzureTableService()
+    table_service.query_moments('test_moment')
     return render(request, 'index.html')
