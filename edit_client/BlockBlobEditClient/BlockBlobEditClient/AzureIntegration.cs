e872223e902f1e3ba6641c988face5d8ada45a30
Vincent Shen <chengwei.shen>
2018-05-26 19:12:37 +0800

    commit

---
+++ edit_client/BlockBlobEditClient/BlockBlobEditClient/AzureIntegration.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace BlockBlobEditClient
+{
+    class AzureIntegration
+    {
+        public const string IMAGE_CONTAINER = "pictureblobs";
+
+        private CloudStorageAccount GetAccount()
+        {
+            if (CloudStorageAccount.TryParse(
+                "DefaultEndpointsProtocol=https;AccountName=timelines;AccountKey=1Xrf8KQwttk+RzGHNBGpMrO6d7llWY6gHpyeSQ+XV6h2N3VEEtTRuSSmvt5+xA3kyQBWND1OAdt8uvMExZz1CA==;EndpointSuffix=core.chinacloudapi.cn",
+                out CloudStorageAccount storagetAccount))
+            {
+                return storagetAccount;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+        
+        private CloudBlobContainer GetBlobContainer(string name)
+        {
+            var account = GetAccount();
+            var client = account.CreateCloudBlobClient();
+
+            var container = client.GetContainerReference(name);
+            container.CreateIfNotExistsAsync().GetAwaiter().GetResult();
+
+            BlobContainerPermissions permissions = container.GetPermissions();
+            // Container 中的所有 Blob 都能被访问
+            permissions.PublicAccess = BlobContainerPublicAccessType.Container;
+            container.SetPermissions(permissions);
+
+            return container;
+        }
+
+        public void UploadImage(string imagePath)
+        {
+            var container = GetBlobContainer(IMAGE_CONTAINER);
+            var localFileName = "EF_" + Guid.NewGuid() + Path.GetExtension(imagePath);
+            var localPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var sourceFile = Path.Combine(localPath, localFileName);
+
+            File.Copy(imagePath, sourceFile);
+
+            CloudBlockBlob cloudBlockBlob = container.GetBlockBlobReference(localFileName);
+            cloudBlockBlob.UploadFromFile(sourceFile);
+        }
+
+        public List<Uri> GetImageList()
+        {
+            var list = new List<Uri>();
+            var container = GetBlobContainer(IMAGE_CONTAINER);
+            BlobContinuationToken blobContinuationToken = null;
+            do
+            {
+                var results = container.ListBlobsSegmented(null, blobContinuationToken);
+                // Get the value of the continuation token returned by the listing call.
+                blobContinuationToken = results.ContinuationToken;
+                foreach (IListBlobItem item in results.Results)
+                {
+                    list.Add(item.Uri);
+                }
+            } while (blobContinuationToken != null);
+
+            return list;
+        }
+    }
+}
---
+++ edit_client/BlockBlobEditClient/BlockBlobEditClient/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Path = System.IO.Path;
+
+namespace BlockBlobEditClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private AzureIntegration _client;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _client = new AzureIntegration();
+            Refresh();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog
+            {
+                DefaultExt = ".jpg",
+                Filter =
+                    @"All Image Files|*.bmp;*.ico;*.gif;*.jpeg;*.jpg;*.png;*.tif;*.tiff|Windows Bitmap(*.bmp)|*.bmp|Windows Icon(*.ico)|*.ico|Graphics Interchange Format (*.gif)|(*.gif)|JPEG File Interchange Format (*.jpg)|*.jpg;*.jpeg|Portable Network Graphics (*.png)|*.png|Tag Image File Format (*.tif)|*.tif;*.tiff"
+            };
+
+            if (dlg.ShowDialog() == true)
+            {
+                var fileName = dlg.FileName;
+                if (!File.Exists(fileName))
+                {
+                    MessageBox.Show("File does not exist.");
+                    return;
+                }
+
+                TxtFile.Text = fileName;
+                DataContext = new { PreviewImgSrc = fileName };
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists(TxtFile.Text))
+            {
+                MessageBox.Show("File does not exist.");
+                return;
+            }
+
+            try
+            {
+                _client.UploadImage(TxtFile.Text.Trim());
+                Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Refresh()
+        {
+            var images = _client.GetImageList();
+            DataContext = new
+            {
+                Images = images
+            };
+        }
+    }
+}
