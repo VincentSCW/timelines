cebcb16e915e12371230e08468530bb9d93d6107
Vincent Shen <chengwei.shen>
2018-05-28 11:12:22 +0800

    fix some bugs

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxEditorModule } from 'ngx-editor';
 
 import { MaterialModule } from './material.module';
 
@@ -19,9 +21,11 @@ import { TopBarComponent } from './top-bar.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxEditorModule
   ],
   exports: [
     MaterialModule
