121073bcf9c62a05f06a8771ff49fd7aee5f0446
Vincent Shen <chengwei.shen>
2018-06-06 15:37:31 +0800

    sidebar content

--- client/src/app/sidebar.component.ts
+++ client/src/app/sidebar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { Timeline } from './models/timeline.model';
+import { TimelineService } from './services/timeline.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html'
 })
 
-export class SidebarComponent {
-  
+export class SidebarComponent implements OnInit {
+  timelines$: Observable<Timeline[]>;
+
+  constructor(private timelineService: TimelineService) {
+
+  }
+
+  ngOnInit() {
+    this.timelines$ = this.timelineService.getTimelines();
+  }
 }
