880b6931263c7df3d671f4e5854358b17054c7f7
Vincent Shen <chengwei.shen>
2018-06-07 08:49:39 +0800

    css changes

--- client/src/app/navbar.component.ts
+++ client/src/app/navbar.component.ts
@@ -2,8 +2,6 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { MomentEditorComponent } from './timeline/moment-editor.component';
 
-import { environment } from '../environments/environment';
-
 @Component({
 	selector: 'app-navbar',
 	templateUrl: './navbar.component.html'
@@ -11,8 +9,6 @@ import { environment } from '../environments/environment';
 
 export class NavbarComponent {
 	@Output() toggleSidenav = new EventEmitter<void>();
-
-	editable = environment.editable;
 
 	constructor(private dialog: MatDialog) {
 
--- client/src/app/sidebar.component.ts
+++ client/src/app/sidebar.component.ts
@@ -3,17 +3,20 @@ import { Location } from '@angular/common';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
+import { environment } from '../environments/environment';
 import { Timeline } from './models/timeline.model';
 import { TimelineService } from './services/timeline.service';
 
 @Component({
   selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html'
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
 })
 
 export class SidebarComponent implements OnInit {
   timelines$: Observable<Timeline[]>;
   urlTopicKey: string;
+  editable = environment.editable;
 
   constructor(private timelineService: TimelineService,
     private router: Router,
