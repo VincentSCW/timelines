a1cd2ea92bc2d159caedc8968ee6c4175e3a4b71
VincentShen <edentidus@foxmail.com>
2018-06-24 19:33:13 +0800

    add timeline, ui ready

--- client/src/app/timeline/timeline-editor.component.ts
+++ client/src/app/timeline/timeline-editor.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Timeline, ProtectLevel, PeriodGroupLevel } from '../models/timeline.model';
 
 @Component({
   selector: 'app-timeline-editor',
   templateUrl: './timeline-editor.component.html'
 })
-export class TimelineEditorComponent {
-  
+export class TimelineEditorComponent implements OnInit {
+  model: Timeline = {
+    topicKey: '',
+    protectLevel: ProtectLevel.public,
+    periodGroupLevel: PeriodGroupLevel.any,
+    isCompleted: false
+  };
+
+  ngOnInit() {
+
+  }
 }
