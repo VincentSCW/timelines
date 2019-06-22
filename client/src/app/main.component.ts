f2cc2587d1141f65671cac59ff0b644a901ff53a
Vincent Shen <edentidus@foxmail.com>
2019-06-22 11:00:46 +0000

    redeisgn main page

--- client/src/app/main.component.ts
+++ client/src/app/main.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TimelineService } from './services/timeline.service';
+import { RecordService } from './services/record.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import { Timeline } from './models/timeline.model';
 
 @Component({
   selector: 'app-main',
-  template: `
-  <div class="notification">
-    <span class="icon">
-      <i class="fas fa-arrow-left"></i>
-    </span>
-    请选择时间轴
-  </div>`
+  templateUrl: 'main.component.html'
 })
-export class MainComponent {
+export class MainComponent implements OnInit, OnDestroy{
+  timelines$: Observable<Timeline[]>;
+  years: number[] = new Array<number>();
   
+  constructor(private timelineService: TimelineService) {
+    this.getYears();
+  }
+
+  ngOnInit() {
+    this.timelines$ = this.timelineService.getTimelines();
+  }
+
+  ngOnDestroy() {
+
+  }
+
+  getYears() {
+    var today = new Date();
+    for (var i = 2017; i <= today.getFullYear(); i++) {
+      this.years.push(i);
+    }
+  }
 }
